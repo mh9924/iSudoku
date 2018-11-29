e005389f6db5d14f336f185b32e4b24de2781069
Matt <xmatth13@live.com>
2018-11-29 14:31:47 -0500

    .

--- tests/test.py
+++ tests/test.py
@@ -2,34 +2,46 @@ from src import Puzzle
 
 
 class SudokuTest:
-    @staticmethod
-    def backtracking():
-        p = Puzzle.Puzzle(
-            [8, None, None, 4, None, 6, None, None, 7,
-            None, None, None, None, None, None, 4, None, None,
-            None, 1, None, None, None, None, 6, 5, None,
-            5, None, 9, None, 3, None, 7, 8, None,
-            None, None, None, None, 7, None, None, None, None,
-            None, 4, 8, None, 2, None, 1, None, 3,
-            None, 5, 2, None, None, None, None, 9, None,
-            None, None, 1, None, None, None, None, None, None,
-            3, None, None, 9, None, 2, None, None, 5])
 
-        p.printSudoku()
+    def openPuzzles(self, dataFileName):
+        puzzles = []
+        lines = open(dataFileName).readlines()
 
-        p.backtrackingSolve()
+        for line in lines:
+            columns = line.split(",")
+            unsolved = columns[0]
+            puzzles.append(unsolved)
 
-        print()
-        print("Solved via backtracking:")
+        return puzzles
 
-        p.printSudoku()
 
-    @staticmethod
-    def crooksAlgo():
+    def backtracking(self):
+        puzzles = self.openPuzzles("sudoku.csv")
+        puzzleObjs = []
+
+        for puzzle in puzzles[:10]:
+            puzzleObjs.append(Puzzle.Puzzle(list(puzzle)))
+
+        print(len(puzzleObjs))
+        print(puzzleObjs[0].cells[2].num)
+
+        for puzzleObj in puzzleObjs:
+            print("------------------------------------")
+            print()
+
+            puzzleObj.printSudoku()
+            puzzleObj.backtrackingSolve()
+
+            print()
+            print("Solved via backtracking:")
+
+            puzzleObj.printSudoku()
+            print()
+
+    def crooksAlgo(self):
         pass
 
-    @staticmethod
-    def dancingLinks():
+    def dancingLinks(self):
         pass
 
 test = SudokuTest()
